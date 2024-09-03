0b3f917cf8def5674b59e3ecb075a1064524d400
Anumesh S. Muthal <muthalanumesh@gmail.com>
2024-09-03 06:02:04 +0000

    Conversion of datatypes(undefine=NaN, Null=0 , sss)

---
+++ Basics/data-types.js
@@ -0,0 +1,34 @@
+// let score = "33abc"
+// console.log(typeof score)
+// console.log(score);
+
+// let numberme = Number(score);
+// console.log(typeof numberme)
+
+
+
+// // Boolean type
+
+// let soft = ""
+// let booleanme = Boolean(soft);
+// console.log(booleanme)
+
+
+// // undefined type
+
+
+// let softy = undefined
+// let undefinedme = Boolean(soft);
+// console.log(typeof undefinedme)
+// console.log(undefinedme)
+
+
+// Null type
+
+let popy = undefined
+console.log(typeof popy)
+
+let opou=Number(popy)
+console.log(opou)
+
+
